0bb181f54518d9d5a7ce1171a01cf0ce5e954223
Mina Mikhail <mina@fightingtheboss.com>
2019-04-07 23:35:06 -0400

    Import TMDB data and render one film title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import TMDB from './TMDB';
 
 const App = () => {
+  const {films} = TMDB;
+
   return (
     <main className="film-library">
       <section className="film-list">
         <h1 className="section-title">FILMS</h1>
+        <h1>{films[0].title}</h1>
       </section>
 
       <section className="film-details">
