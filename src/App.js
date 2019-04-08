d58e3e466a6af2bf7d3a59982bc2dba7d06512c0
Mina Mikhail <mina@fightingtheboss.com>
2019-04-07 23:47:46 -0400

    Render all film rows by mapping over films collection

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import TMDB from './TMDB';
 const App = () => {
   const {films} = TMDB;
 
+  const filmRows = films.map((film) => {
+    return (
+      <article className="film-row" key={film.id}>
+        <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
+        <div className="film-summary">
+          <h1>{film.title}</h1>
+          <p>{new Date(film.release_date).getFullYear()}</p>
+        </div>
+      </article>
+    );
+  });
+
   return (
     <main className="film-library">
       <section className="film-list">
         <h1 className="section-title">FILMS</h1>
-        <article className="film-row">
-          <img src={`https://image.tmdb.org/t/p/w780/${films[0].poster_path}`} alt={films[0].title} />
-          <div className="film-summary">
-            <h1>{films[0].title}</h1>
-            <p>{new Date(films[0].release_date).getFullYear()}</p>
-          </div>
-        </article>
+        {filmRows}
       </section>
 
       <section className="film-details">
