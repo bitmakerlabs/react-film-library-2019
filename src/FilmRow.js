d70ba74b80becd70c265c887a1e8c4f0a7567194
Mina Mikhail <mina@fightingtheboss.com>
2019-04-08 00:38:21 -0400

    Add placeholder event handler for fetching film details

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -3,8 +3,12 @@ import FilmPoster from './FilmPoster';
 import Fave from './Fave';
 
 const FilmRow = ({film}) => {
+  const handleDetailsClick = (event) => {
+    console.log(`Fetching details for ${film.title}`);
+  }
+
   return (
-    <article className="film-row">
+    <article className="film-row" onClick={handleDetailsClick}>
       <FilmPoster film={film} />
       <div className="film-summary">
         <h1>{film.title}</h1>
