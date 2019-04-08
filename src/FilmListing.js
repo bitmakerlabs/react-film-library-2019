5506a1870437858a7718861b327bd7bd1823439f
Mina Mikhail <mina@fightingtheboss.com>
2019-04-08 00:45:17 -0400

    Add filters to FilmListing

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -11,6 +11,18 @@ const FilmListing = ({films}) => {
   return (
     <section className="film-list">
       <h1 className="section-title">FILMS</h1>
+
+      <nav className="film-list-filters">
+        <button className="film-list-filter">
+          ALL
+          <span className="section-count">{films.length}</span>
+        </button>
+        <button className="film-list-filter">
+          FAVES
+          <span className="section-count">{0}</span>
+        </button>
+      </nav>
+
       {filmRows}
     </section>
   );
