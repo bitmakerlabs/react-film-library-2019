f6ba7888e739753c47b676cd436d5bb41a12a16e
Mina Mikhail <mina@fightingtheboss.com>
2019-04-08 00:47:53 -0400

    Add placeholder event handler for filter toggle on FilmListing

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -8,16 +8,20 @@ const FilmListing = ({films}) => {
     );
   });
 
+  const handleFilterToggle = (filter) => {
+    console.log(`Setting filter to ${filter}`);
+  }
+
   return (
     <section className="film-list">
       <h1 className="section-title">FILMS</h1>
 
       <nav className="film-list-filters">
-        <button className="film-list-filter">
+        <button className="film-list-filter" onClick={() => handleFilterToggle('all')}>
           ALL
           <span className="section-count">{films.length}</span>
         </button>
-        <button className="film-list-filter">
+        <button className="film-list-filter" onClick={() => handleFilterToggle('faves')}>
           FAVES
           <span className="section-count">{0}</span>
         </button>
