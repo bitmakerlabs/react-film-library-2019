18f6404aaa49f2889058d13c438d3516fb248730
Mina Mikhail <mina@fightingtheboss.com>
2019-04-07 23:52:52 -0400

    Extract FilmRow into its own component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import TMDB from './TMDB';
+import FilmRow from './FilmRow';
 
 const App = () => {
   const {films} = TMDB;
 
   const filmRows = films.map((film) => {
     return (
-      <article className="film-row" key={film.id}>
-        <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
-        <div className="film-summary">
-          <h1>{film.title}</h1>
-          <p>{new Date(film.release_date).getFullYear()}</p>
-        </div>
-      </article>
+      <FilmRow film={film} />
     );
   });
 
---
+++ src/FilmRow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FilmRow = ({film}) => {
+  return (
+    <article className="film-row" key={film.id}>
+      <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
+      <div className="film-summary">
+        <h1>{film.title}</h1>
+        <p>{new Date(film.release_date).getFullYear()}</p>
+      </div>
+    </article>
+  );
+};
+
+export default FilmRow;
