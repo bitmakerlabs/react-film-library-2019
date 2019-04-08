04c13d034f29927d40a799c4a023179aea846f3e
Mina Mikhail <mina@fightingtheboss.com>
2019-04-07 23:30:40 -0400

    Add basic layout for the app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 const App = () => {
   return (
     <main className="film-library">
-      APP GOES HERE
+      <section className="film-list">
+        <h1 className="section-title">FILMS</h1>
+      </section>
+
+      <section className="film-details">
+        <h2 className="section-title">DETAILS</h2>
+      </section>
     </main>
   );
 }
