0b6cd8526b21d6271b576ca8599d7c009ffa93a7
Mina Mikhail <mina@fightingtheboss.com>
2019-04-08 00:35:19 -0400

    Add placeholder event handler for Fave

--- src/Fave.js
+++ src/Fave.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Fave = () => {
+  const handleClick = (event) => {
+    event.stopPropagation();
+    console.log('Handling Fave click!');
+  };
+
   return (
-    <button className="film-row-fave add_to_queue">
+    <button className="film-row-fave add_to_queue" onClick={handleClick}>
       <i className="material-icons">add_to_queue</i>
     </button>
   );
