be6ed46277ff62664a40d088d870987696e7614a
Mina Mikhail <mina@fightingtheboss.com>
2019-04-08 00:02:31 -0400

    Extract FilmPoster to its own component

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import FilmPoster from './FilmPoster';
 
 const FilmRow = ({film}) => {
   return (
     <article className="film-row">
-      <img src={`https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title} />
+      <FilmPoster film={film} />
       <div className="film-summary">
         <h1>{film.title}</h1>
         <p>{new Date(film.release_date).getFullYear()}</p>
